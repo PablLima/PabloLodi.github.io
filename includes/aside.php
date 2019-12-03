69ad9ed288d7a2da754ca1fc89180809cfd9c21c
Pablo Lodi <lodiricardopablo@gmail.com>
2019-12-03 01:15:04 -0200

    ajax desativado

--- includes/aside.php
+++ includes/aside.php
@@ -1,36 +1,36 @@
 <aside>
-    <nav>
-        <ul class="access">
-            <li>
-                <a href="#" onclick="ajaxReload('animais.php')">
-                    <i class="fas fa-paw"></i><br>
-                    <p>Animais</p>
-                </a>
-            </li>
-            <li>
-                <a href="#" onclick="ajaxReload('alimentacao.php')">
-                    <i class="fas fa-utensils"></i><br>
-                    <p>Alimentação</p>
-                </a>
-            </li>
-            <li>
-                <a href="#" onclick="ajaxReload('insumos.php')">
-                    <i class="fas fa-bacon"></i><br>
-                    <p>Insumos</p>
-                </a>
-            </li>
-            <li>
-                <a href="#" onclick="ajaxReload('lotes.php')">
-                    <i class="fas fa-car"></i><br>
-                    <p>Lotes</p>
-                </a>
-            </li>
-            <li>
-                <a href="#" onclick="ajaxReload('estatisticas.php')">
-                    <i class="fas fa-chart-pie"></i><br>
-                    <p>Estatísticas</p>
-                </a>
-            </li>				
-        </ul>
-    </nav>
-</aside>
+		<nav>
+			<ul class="access">
+				<li>
+					<a href="animais.php">
+						<i class="fas fa-paw"></i><br>
+						<p>Animais</p>
+					</a>
+				</li>
+				<li>
+					<a href="alimentacao.php">
+						<i class="fas fa-utensils"></i><br>
+						<p>Alimentação</p>
+					</a>
+				</li>
+				<li>
+					<a href="insumos.php">
+						<i class="fas fa-bacon"></i><br>
+						<p>Insumos</p>
+					</a>
+				</li>
+				<li>
+					<a href="lotes.php">
+						<i class="fas fa-car"></i><br>
+						<p>Lotes</p>
+					</a>
+				</li>
+				<li>
+					<a href="estatisticas.php">
+						<i class="fas fa-chart-pie"></i><br>
+						<p>Estatísticas</p>
+					</a>
+				</li>				
+			</ul>
+		</nav>
+	</aside>
--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <header class="access">
-    <h2><a href="#" onclick="ajaxReload('index.php')">Milk Manager 2019</a></h2>
+    <h2><a href="index.php">Milk Manager 2019</a></h2>
     <a href="adm/cadastro.php">Usuários</a>
     <a href="preferencias.php">Preferências</a>
     <a href="logoff.php">Fazer logoff</a>
